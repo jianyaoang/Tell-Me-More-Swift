df32ebe7c53ff153caeac7bc590c7069e05579ad
jianyaoang <jianyaoang1@gmail.com>
2014-10-26 18:54:23 +0800

    Setting labels, created functions, Prediction action via UIButton, UIColor for background and text color

--- Tell Me More/ViewController.swift
+++ Tell Me More/ViewController.swift
@@ -10,16 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var introLabel: UILabel!
+    @IBOutlet var predictButton: UIButton!
+    @IBOutlet var predictionLabel: UILabel!
+
+    let thePredictions = prediction()
+    
+    @IBAction func onPredictButtonPressed(sender: AnyObject) {
+        
+        retrievingPredictions()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
+        self.view.backgroundColor = UIColor(white: 0.5, alpha: 0.5)
         
+        self.introLabel.text = "Welcome to Tell Me More"
+        self.introLabel.textColor = UIColor.whiteColor()
+        
+        self.predictionLabel.text = "Press on predict button"
+        self.predictionLabel.textColor = UIColor.whiteColor()
+        
+        self.predictButton.setTitle("Predict", forState: .Normal)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-
         
     }
+    
+    func retrievingPredictions(){
+        
+        let predictionIndex: Int = Int(arc4random()) % thePredictions.predictionArray.count
+
+        self.predictionLabel.numberOfLines = 0
+        self.predictionLabel.text = thePredictions.predictionArray.objectAtIndex(predictionIndex) as NSString
+        
+    }
+    
 }
+
+
+
+
 
